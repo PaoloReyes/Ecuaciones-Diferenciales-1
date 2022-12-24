bd855da9854f4b4daacdec9cba35eb65cab53810
Paolo Reyes <paolo.alfonso.reyes@gmail.com>
2022-12-24 10:31:44 -0600

    Firsts Scenes

---
+++ main.py
@@ -0,0 +1,31 @@
+'''
+    Código para el video "¿Qué son las ecuaciones diferenciales?" de Paolo Inspires
+    Copyright (C) 2022 por Paolo Alfonso Reyes Ramírez
+'''
+
+# Librerias
+from manim import *
+from manim_voiceover import VoiceoverScene
+from manim_voiceover.services.gtts import GTTSService
+from manim_voiceover.services.recorder import RecorderService
+from intro import Intro
+from que_son import QueSon
+from categorias import Categorias
+from tipos import Tipos
+
+# Configuración Global
+config.background_color = WHITE
+Text.set_default(color=BLACK)
+Tex.set_default(color=BLACK)
+MathTex.set_default(color=BLACK)
+
+# Escena Principal
+class Main(VoiceoverScene):
+    def construct(main_scene):
+        main_scene.set_speech_service(RecorderService())
+        Intro.construct(main_scene)
+        main_scene.wait()
+        QueSon.construct(main_scene)
+        main_scene.wait(0.5)
+        Categorias.construct(main_scene)
+        Tipos.construct(main_scene)
